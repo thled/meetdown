aefbb577944ab1cb0d0b1d6336fa241b6835ef39
Thomas Le Duc <dev@tleduc.de>
2021-08-24 08:23:16 +0200

    Add health route with check action

---
+++ app/src/Controller/HealthCheck.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HealthCheck extends AbstractController {
+    #[Route("/health", name: "health_check")]
+    public function check(): JsonResponse {
+        return $this->json([]);
+    }
+}
