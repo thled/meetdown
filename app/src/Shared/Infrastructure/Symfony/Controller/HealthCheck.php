49b7cf0104f39d061e282e9fae028048fbb3ebe0
Thomas Le Duc <dev@tleduc.de>
2021-08-24 09:10:44 +0200

    Move HealthCheck controller to Shared bounded context

--- app/src/Shared/Infrastructure/Symfony/Controller/HealthCheck.php
+++ app/src/Shared/Infrastructure/Symfony/Controller/HealthCheck.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace MeetDown\Controller;
+namespace MeetDown\Shared\Infrastructure\Symfony\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
