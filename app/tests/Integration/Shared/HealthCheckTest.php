2af2fb38045eb00ceb2553ac6fd7d8519a496736
Thomas Le Duc <dev@tleduc.de>
2021-08-24 09:18:51 +0200

    Fix style and static errors

--- app/tests/Integration/Shared/HealthCheckTest.php
+++ app/tests/Integration/Shared/HealthCheckTest.php
@@ -3,18 +3,20 @@
 namespace Tests\Integration\Shared;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use function json_encode;
 
 final class HealthCheckTest extends WebTestCase {
     /** @test */
     public function it_is_healthy(): void {
-        $client = static::createClient();
+        $client = self::createClient();
 
         $client->request("GET", "/health");
 
         $response = $client->getResponse();
         self::assertSame(200, $response->getStatusCode());
         self::assertTrue($response->headers->contains("Content-Type", "application/json"));
-        self::assertJsonStringEqualsJsonString("{\"status\": \"up\"}", $response->getContent());
+        assert(is_string($response->getContent()));
+        self::assertJsonStringEqualsJsonString(<<<JSON
+            {"status": "up"}
+            JSON, $response->getContent());
     }
 }
